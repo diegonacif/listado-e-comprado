0d6faa911c99c5ddc9e456c283c1f13548ec5c6e
diegonacif <diegoreisnacif@gmail.com>
2023-03-09 23:39:54 -0300

    feat: saving input data in array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './css/App.scss';
 
 export const App = () => {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState('');
+
+  function handleAddProduct() {
+    setData(current => [...current, input]);
+    setInput('');
+  }
+
+  console.log(data)
 
   return (
     <div className="App">
-      <h1>teste</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => handleAddProduct()}>Adicionar</button>
+      {
+        data.map((data) => {
+          return <p>{data}</p>
+        })
+      }
     </div>
   )
 }
